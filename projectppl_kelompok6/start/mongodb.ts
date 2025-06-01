cb6dc5a655d469c1f5fdcbbcb42f624e55ef02c9
Alhusna Hanifah <alhusna5@mhs.usk.ac.id>
2025-06-01 22:48:36 +0700

    add mongodb atlas

--- projectppl_kelompok6/start/mongodb.ts
+++ projectppl_kelompok6/start/mongodb.ts
@@ -5,15 +5,17 @@ import env from '#start/env' // ✅ gunakan alias jika sudah diset
 let db: Db
 
 export async function connectMongo(): Promise<void> {
-  const client = new MongoClient(env.get('MONGO_URI'))
+  const uri = env.get('MONGO_URI')
+  const client = new MongoClient(uri, {
+    tls: true, // wajib untuk Atlas
+    retryWrites: true, // optional, biasanya sudah di URI
+  })
+
   await client.connect()
   db = client.db(env.get('MONGO_DB_NAME'))
   console.log('✅ MongoDB connected')
 }
 
 export function getMongoDb(): Db {
-  if (!db) {
-    throw new Error('❌ MongoDB not connected. Please call connectMongo() first.')
-  }
   return db
 }
